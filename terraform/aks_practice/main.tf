56e144dd29dfc99873c731e0f00489d6a678071f
Srini <srinivasann.career@gmail.com>
2025-11-17 09:11:27 +0530

    update vm size as per free tier

--- terraform/aks_practice/main.tf
+++ terraform/aks_practice/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "system"
     node_count = var.node_count
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B1s"
   }
 
   identity { type = "SystemAssigned" }
