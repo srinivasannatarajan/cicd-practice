cc84b1d951169b37156045d40c7d18b09690dc85
Srini <srinivasann.career@gmail.com>
2025-11-17 08:55:41 +0530

    add test aks

---
+++ terraform/aks_practice/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "practicedns"
+
+  default_node_pool {
+    name       = "system"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity { type = "SystemAssigned" }
+}
